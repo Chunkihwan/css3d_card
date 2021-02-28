383e052c623ed701b40614bb369a768c85ceadb2
kihwan Chun <yahao2512@gmail.com>
2021-02-28 15:54:32 +0900

    수정

--- js/motion.js
+++ js/motion.js
@@ -1,51 +1,49 @@
 var wrap;
-let x=0,y=0;
-let mx =0,my = 0;
-let orientNumX = 0;
-let orientNumY = 0;
-let isMobile = false;
-let perNum = 10;
+var x = 0,
+    y = 0;
+var mx = 0,
+    my = 0;
+var orientNumX = 0;
+var orientNumY = 0;
+var isMobile = false;
 
-window.onload = function(){
+window.onload = function () {
     wrap = document.querySelector(".contentWrap");
     isMobile = mobileChk();
-    if(isMobile){
-        perNum = 1;
+
+    if (isMobile) {
+        //모바일이면 실행
         if (window.DeviceOrientationEvent) {
-            window.addEventListener("deviceorientation", function(event){
+            window.addEventListener("deviceorientation", function (event) {
                 x = event.gamma;
-                y = event.beta ;
+                y = event.beta;
             });
         }
         loopMobile();
 
-    }else{
-        window.addEventListener("mousemove", function(e){
+    } else {
+        //pc면 실행
+        window.addEventListener("mousemove", function (e) {
             x = (e.clientX - window.innerWidth / 2);
             y = (e.clientY - window.innerHeight / 2);
         });
         loop();
     }
-    
+
 }
 
-function loop(){
+function loopMobile() {
     mx += (x - mx) * .1;
-    my += (y - my) * .1; 
-
-    wrap.style.transform = "translate3d(-50%, -50%, 0) rotateX("+ (my / perNum) +"deg) rotateY("+ (-mx / perNum ) +"deg)";
-    window.requestAnimationFrame(loop);
-}
-
-var tempx= 0, tempy=0;
-function loopMobile(){
-    mx += (x - mx) * .1;
-    my += (y - my) * .1; 
-    // tempx -= .01;
-    // tempy += .4;
-    // console.log(tempx)
-    wrap.style.transform = "translate3d(-50%, -50%, 0) rotateX("+ (my- 50) +"deg) rotateY("+ (mx) +"deg)";
+    my += (y - my) * .1;
+    wrap.style.transform = "translate3d(-50%, -50%, 0) rotateX(" + (my - 50) + "deg) rotateY(" + (mx) + "deg)";
     window.requestAnimationFrame(loopMobile);
+}
+
+function loop() {
+    mx += (x - mx) * .1;
+    my += (y - my) * .1;
+    wrap.style.transform = "translate3d(-50%, -50%, 0) rotateX(" + (my / 10) + "deg) rotateY(" + (-mx / 10) + "deg)";
+    window.requestAnimationFrame(loop);
 }
 
 
